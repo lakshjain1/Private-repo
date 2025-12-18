813c4d9afe6be3a7b30dd499cbd425a1900e39d8
lakshjain1 <lakshjain0498@gmail.com>
2025-12-18 11:05:22 +0530

    added new components

---
+++ redis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash 
+
+#She bang line
+
+ID=$(id -u) # Get user ID if it is 0 then root user
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+
+VALIDATE $? "Remi Repo Installation"
+
+yum module enable redis:remi-6.2 -y &>> $LOGFILE
+
+VALIDATE $? "Enable Redis Module"
+
+yum install redis -y &>> $LOGFILE
+
+VALIDATE $? "Redis Installation"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
+
+VALIDATE $? "Updating Redis Configuration"
+
+systemctl enable redis &>> $LOGFILE
+
+VALIDATE $? "Enable Redis Service"
+
+systemctl start redis &>> $LOGFILE
+
+VALIDATE $? "Start Redis Service"
+
+echo -e "$G Redis Installation Completed Successfully $N"
---
+++ web.sh
@@ -0,0 +1,70 @@
+#!/bin/bash 
+
+#She bang line
+
+ID=$(id -u) # Get user ID if it is 0 then root user
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install nginx -y &>> $LOGFILE
+
+VALIDATE $? "Nginx Installed"
+
+systemctl enable nginx &>> $LOGFILE
+
+VALIDATE $? "Nginx Enabled"
+
+systemctl start nginx &>> $LOGFILE
+
+VALIDATE $? "Nginx Started"
+
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+
+VALIDATE $? "Nginx Default content removed"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> $LOGFILE
+
+VALIDATE $? "Web content downloaded"
+
+cd /usr/share/nginx/html &>> $LOGFILE
+
+VALIDATE $? "Changed directory to nginx html"
+
+unzip /tmp/web.zip &>> $LOGFILE
+
+VALIDATE $? "Web content unzipped"
+
+cp /home/centos/Private-repo/roboshop.conf /etc/nginx/conf.d/roboshop.conf &>> $LOGFILE
+
+VALIDATE $? "Nginx config copied"
+
+systemctl restart nginx &>> $LOGFILE
+
+VALIDATE $? "Nginx restarted"
+
+echo -e "$G Web Server Setup Completed $N"
