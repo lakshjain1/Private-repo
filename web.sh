eba7c3a6c1042859631a37b71283c9a9416782c4
lakshjain1 <lakshjain0498@gmail.com>
2025-12-18 13:22:57 +0530

    edit components

--- web.sh
+++ web.sh
@@ -47,7 +47,7 @@ rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 
 VALIDATE $? "Nginx Default content removed"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> $LOGFILE
+curl -o /tmp/web.zip https://roboshop-artifacts.s3.amazonaws.com/web.zip &>> $LOGFILE
 
 VALIDATE $? "Web content downloaded"
 
